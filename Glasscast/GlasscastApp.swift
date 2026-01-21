f865fd645a614667bb1d8c6d0c17fc15601092dd
Jatin Singhroha <jatin.kumar@vijayi-wfh.com>
2026-01-21 17:27:49 +0530

    Initial Commit

---
+++ Glasscast/GlasscastApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlasscastApp.swift
+//  Glasscast
+//
+//  Created by Jatin Singhroha on 21/01/26.
+//
+
+import SwiftUI
+
+@main
+struct GlasscastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlasscastTests/GlasscastTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlasscastTests.swift
+//  GlasscastTests
+//
+//  Created by Jatin Singhroha on 21/01/26.
+//
+
+import Testing
+@testable import Glasscast
+
+struct GlasscastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
